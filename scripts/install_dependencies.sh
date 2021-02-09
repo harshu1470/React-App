0488060da44e98404c5912793ef7c2cc3f55ee11
Harshit Sharma <35574904+harshu1470@users.noreply.github.com>
2021-02-09 13:14:38 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo yum update
-sudo yum install -y curl 
-curl -sL https://rpm.nodesource.com/setup_12.x | bash -
-sudo yum install -y nodejs
+#sudo yum install -y curl 
+#curl -sL https://rpm.nodesource.com/setup_12.x | bash -
+#sudo yum install -y nodejs
 sudo yum install -y nginx 
